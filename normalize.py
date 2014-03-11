f0eaf97d2ff0f33570e8c9538ec477c586a10500
apaw <apaw@ucdavis.edu>
2014-03-10 23:29:54 -0700

    adding in normalized data

---
+++ normalize.py
@@ -0,0 +1,17 @@
+from pyparsing import *
+
+data_file = open("email-Enron.txt")
+
+grammar = Word(nums) + Word(nums)
+
+buffer = ""
+
+for line in data_file:
+	buffer += line
+	match = next(grammar.scanString(buffer), None)
+	while match:
+		tokens, start, end = match
+		if (tokens[0] < tokens[1]):
+			print tokens[0] + ' ' + tokens[1]
+		buffer = buffer[end:]
+		match = next(grammar.scanString(buffer), None)
